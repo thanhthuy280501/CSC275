f76dbced2bda9bdababa8aab9d9867ddad7be3eb
thanhthuy280501 <156226788+thanhthuy280501@users.noreply.github.com>
2024-09-07 22:00:59 -0400

    TN ic01.cpp

--- ic01.cpp
+++ ic01.cpp
@@ -5,30 +5,41 @@ using namespace std;
 // In-class 01.
 // (Partial) Author:	S. Turner
 // Due date:		(in-class, 09/01/22)
+// Name: Thuy Nguyen
+// Class: CSC 275
 //***************************************************************************
 
 #define MAX 400
 
 class CounterType {
-  public:
-    void InitializeCounter();	// complete this function!
-
-    void increment() {		// As Keyshia Cole or Jerry Maguire
-    				// might say, "You complete me."
-    }
-
-    void decrement() {		// did I happen to mention, you need to
-				// complete this function?
-    }
-
-// You need to modify this:
-  int currentCount() {		// I was the son of "complete this function"
-
-  }
-
-  private:
+public:
+void InitializeCounter();    			// Function to initialize the counter to 0.
+void increment(); 				// Function to increment the counter by 1.
+void decrement(); 				// Function to decrement the counter by 1 if it's greater than 0.
+int curretnCount(); 				// Function to return the current value of the counter. 
+   
+private:
     int count;
 };
+
+void CounterType::InitializeCounter() {
+	count = 0;				// Initialize the counter to 0.
+ } 
+
+void CounterType::increment() {
+	    count++;				// Increment the counter by 1.
+    }
+
+void CounterType::decrement() {
+	    if (count > 0){
+		count--;			// Decrement the counter by 1 if it's greater than 0.
+	    }
+    }
+
+  int CounterType:: currentCount() {
+	  return count;				// Return the current value of the counter.
+
+  }
 
 int main() {
 //******************************************
@@ -36,16 +47,29 @@ int main() {
 // Also - it must make a call to its 
 // 'initialize counter' function.
 //******************************************
+	CounterType localCounter; 		// Declare an instance of CounterType.
+
+	localCounter.InitializeCounter(); 	// Initialize the counter to 0.
+
+	int divisibleByThreeCount = 0;
+	
 
   //************************************************************************
   // If the loop index is divisible by 3, then increment the localCounter.
   //************************************************************************
   for(int i = 1; i < MAX; i++) {
+	  if (i % 3 ==0){
+		  localCounter.increment();
+		  divisibleByThreeCount++;
+	  }
 
   }
   
   // Use cout statement to print out a message of the form
   //   "There are 342 numbers between 1 and 343123 that are divisible by 3."
+	
+cout >> "There are" << divisibleByThreeCount << "numbers between 1 and " << MAX 
+		<< " that are divisible by 3. " << endl;
 
 
 //***************************************************************************
@@ -59,11 +83,9 @@ int main() {
 // (The first one is the counter, being decremented. The second one is the
 // value that's divisible by 3.)
 //***************************************************************************
-  for(int i = 1; i < MAX; i++)
-  {
-    if (the loop variable is divisible by three) {	// more code
-      cout << localCounter.currentCount(); 		// to finish
-      cout << " -- " << i << endl;
+  for(int i = 1; i < MAX; i++){
+    if (i % 3 ==0) {	
+      cout << localCounter.currentCount() << " -- " << i << endl;
       localCounter.decrement();
     }
   }
